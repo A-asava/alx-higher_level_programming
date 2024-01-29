b83423c84ab819013a1e7fd54fffb8f33190b5c4
A-asava <adikasaid996@gmail.com>
2024-01-29 19:12:29 +0000

    definition of a rectangle

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+
+class Rectangle:
+    """Representing the class rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializing the class
+        Args:
+            width: width size of the rectangle
+            height: height size of the rectangle
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Retrieves the width"""
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve the height"""
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Returns the height of the rectangle"""
+
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2*(self.__width + self.__height))
+
+    def __str__(self):
+        """Returns the printable representation of the rectangle"""
+
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        new_rect = []
+        for i in range(self.__height):
+            for j in range(self.__width):
+                new_rect.append('#')
+            if i != self.__height - 1:
+                new_rect.append("\n")
+
+        return ("".join(new_rect))
